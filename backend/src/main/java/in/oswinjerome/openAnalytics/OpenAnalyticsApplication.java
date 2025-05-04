decf29e849b937d2db6b8ea41ea37ba45004c613
Oswin Jerome <oswinjeromej@gmail.com>
2025-05-04 19:57:06 +0530

    Initial

---
+++ backend/src/main/java/in/oswinjerome/openAnalytics/OpenAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package in.oswinjerome.openAnalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAnalyticsApplication.class, args);
+	}
+
+}
