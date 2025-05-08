7956b52f3b8ebb4580962a570778f3a1b663837e
Manoj <anoj9626@gmail.com>
2025-05-08 17:45:49 +0530

    Register page (a tag to link changed ,logo placed and style changed)

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -1,13 +1,29 @@
 import RegisterForm  from "@/components/register-form";
+import Link from "next/link";
+import { ChartArea } from 'lucide-react';
+import { unstable_ViewTransition as ViewTransition } from "react";
 
 const RegisterPage = () => {
   return (
     <div >
     <div className="min-h-svh grid lg:grid-cols-2">
-      <div className="bg-[var(--color-app-500)]"></div>
-      <div className="flex flex-col gap-6 justify-center items-center p-4">
+      <div className="bg-[var(--color-app-500)] relative hidden lg:block">
+      </div>
+      <div className="flex flex-col gap-6  p-4 w-full">
+          <div className="p-4 flex justify-start">     
+             <Link href="/" className="flex items-center gap-2 font-medium">
+              <ViewTransition name="logo-transition">
+                <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+                  <ChartArea className="size-4" />
+                </div>
+                <span>OpenAnalytics</span>
+              </ViewTransition>
+            </Link>
+            </div>
+        <div className=" h-full w-full flex items-center justify-center">
         <div className=" max-w-xs w-full">
         <RegisterForm/>
+        </div>
         </div>
       </div>
     </div>
