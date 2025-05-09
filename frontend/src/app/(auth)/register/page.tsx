5d2e0478ac5163f74dbcb1a6ce5bed33b7d5311e
Manoj <anoj9626@gmail.com>
2025-05-09 18:58:47 +0530

    Style changed in Register page

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -6,15 +6,15 @@ import { unstable_ViewTransition as ViewTransition } from "react";
 const RegisterPage = () => {
   return (
     <div >
-    <div className="min-h-svh grid lg:grid-cols-2">
+    <div className="min-h-svh grid lg:grid-cols-2 text-primary">
       <div className="bg-[var(--color-app-500)] relative hidden lg:block">
       </div>
-      <div className="flex flex-col gap-6  p-4 w-full">
-          <div className="p-4 flex justify-start">     
+      <div className="flex flex-col gap-4 p-4 w-full">
+          <div className="p-4 flex justify-center md:justify-start">     
              <Link href="/" className="flex items-center gap-2 font-medium">
               <ViewTransition name="logo-transition">
                 <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-                  <ChartArea className="size-4" />
+                  <ChartArea className="size-4" /> 
                 </div>
                 <span>OpenAnalytics</span>
               </ViewTransition>
