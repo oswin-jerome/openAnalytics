55cce5e005a0245382c6520d534aa7723c34187d
Manoj <anoj9626@gmail.com>
2025-05-10 21:12:18 +0530

    create page added

--- frontend/src/app/(landing)/projects/create/page.tsx
+++ frontend/src/app/(landing)/projects/create/page.tsx
@@ -7,7 +7,7 @@ export default function(){
         <div className='w-full h-full p-6 flex justify-center items-center  '>
         <div className="w-full max-w-4xl h-[70%]  p-4 flex flex-col  gap-3 rounded-2xl bg-gray-200">
             <div className="p-4 w-full  text-xl text-primary  font-bold rounded-lg text-start "> New Project</div>
-            <div className="w-full h-full  ">
+            <div className="w-full h-full ">
             <CreateFrom/>
             </div>
         </div>
